83a15b2e2417cb13298fdbec809a0a2e660430b3
Marthin <martin.pakpahan@edgeprop.sg>
2021-12-02 21:56:31 +0700

    add comments count in topic index

--- app/Topic.php
+++ app/Topic.php
@@ -14,10 +14,21 @@ class Topic extends Model
      */
     protected $fillable = ['user_id', 'title', 'content', 'status'];
 
-    protected $visible = ['id', 'user_id', 'title', 'content', 'status', 'created_at', 'updated_at', 'user'];
+    protected $visible = ['id', 'user_id', 'title', 'content', 'status', 'created_at', 'updated_at', 'user', 'commentsCount'];
+
+    protected $appends = ['commentsCount'];
 
     public function user() {
         return $this->hasOne(User::class, 'id', 'user_id');
+    }
+
+    public function comment() {
+        return $this->hasMany(Comment::class, 'topic_id');
+    }
+
+    public function getCommentsCountAttribute()
+    {
+        return $this->comment()->count();
     }
 
 }
